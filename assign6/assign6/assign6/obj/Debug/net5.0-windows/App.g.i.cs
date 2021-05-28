9e88cd7b3e7857bc2e9c56529880d4b8903a81e7
ahmad ar <aa224fn@student.lnu.se>
2021-05-28 16:58:50 +0200

    last commint

--- assign6/assign6/assign6/obj/Debug/net5.0-windows/App.g.i.cs
+++ assign6/assign6/assign6/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace assign6 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace assign6 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public static void Main() {
             assign6.App app = new assign6.App();
             app.InitializeComponent();
